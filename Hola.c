a13513e8ff528b1a9ad5b09252f699e83e528757
xXomoXx <44613846+xXomoXx@users.noreply.github.com>
2023-09-07 17:05:23 +0200

    added Hola.c and Vagrantfile

---
+++ Hola.c
@@ -0,0 +1,8 @@
+// Simple Hello World program
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+	printf("\n\t-------------\n\tHello World!\n\t-------------\n");
+	printf("\n\t---------------------\n\tHello Freital as well\n\t---------------------\n\n");
+	return 0;
+}
