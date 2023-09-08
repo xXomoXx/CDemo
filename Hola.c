e74e9ab5d87187f1ed2e3357cfb0366c1f6068e9
xXomoXx <44613846+xXomoXx@users.noreply.github.com>
2023-09-08 12:18:28 +0200

    C Code modified

--- Hola.c
+++ Hola.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-	printf("\n\t-------------\n\tHello World! am 08.09.2023 um 11:27 Uhr\n\t-------------\n");
+	printf("\n\t-------------\n\tHello World! am 08.09.2023 um 12:18 Uhr\n\t-------------\n");
 //	printf("\n\t---------------------\n\tHello Freital as well\n\t---------------------\n\n");
 	return 0;
 }
